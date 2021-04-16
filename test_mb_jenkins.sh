ac6312c8a077838be7a56ad604236792ff6b2dc9
m2ijpelissier <82088966+m2ijpelissier@users.noreply.github.com>
2021-04-16 14:04:34 +0200

    Update test_mb_jenkins.sh

--- test_mb_jenkins.sh
+++ test_mb_jenkins.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-echo "build le : `date +'%D'`" >> /home/jenkins/test_mb_jenkins.log
+date_jour = date +'%D'
+date_heure = date +'%h%m'
+echo "build le : "$date_jour" à "$date_heure >> /home/jenkins/test_mb_jenkins.log
